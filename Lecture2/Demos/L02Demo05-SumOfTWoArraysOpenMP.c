7b5396a05582456eef0e506431a9b41e719730f8
GeorgeVovos <gvovos@gmail.com>
2024-10-24 13:15:14 +0100

    Added Lecture two demos in OpenMP

---
+++ Lecture2/Demos/L02Demo05-SumOfTWoArraysOpenMP.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 5
+
+int main() {
+   srand(time(0));
+
+   int *A = malloc(N * sizeof(int));
+   int *B = malloc(N * sizeof(int));
+   int *C = malloc(N * sizeof(int));
+
+
+   for (int i = 0; i < N; i++)
+   {
+      A[i] = rand() % 10;
+      B[i] = rand() % 10;
+   }
+
+   #pragma omp parallel for
+   for (int i =0; i <N; i++)
+      C[i] = A[i] + B[i];
+
+   return 0;
+}
