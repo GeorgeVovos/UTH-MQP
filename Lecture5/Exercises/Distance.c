9c8bed5ca6883ec2aaa111ba39785d8ae100e40c
GeorgeVovos <George.Vovos@prometric.com>
2024-11-05 16:36:18 +0000

    Just combined reductions in 1 for loop

--- Lecture5/Exercises/Distance.c
+++ Lecture5/Exercises/Distance.c
@@ -13,20 +13,20 @@ int main()
     int *B = malloc(N * sizeof(int));
 
     // INIT
-    for (int i = 0; i < N; i++) {
-            A[i] = rand() % 10;
-            B[i] = rand() % 10;
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = rand() % 10;
+        B[i] = rand() % 10;
     }
 
     // Part 1
     double aValue = 0.0, bValue = 0.0, angle = 30.0;
-    #pragma omp parallel for reduction(+ : aValue)
+    #pragma omp parallel for reduction(+ : aValue) reduction(+ : bValue)
     for (int parallel_i = 0; parallel_i < N; parallel_i++)
+    {
         aValue += (A[parallel_i] * A[parallel_i]);
-
-    #pragma omp parallel for reduction(+ : bValue)
-    for (int parallel_i = 0; parallel_i < N; parallel_i++)
         bValue += (B[parallel_i] * B[parallel_i]);
+    }
 
     aValue = sqrt(aValue);
     bValue = sqrt(bValue);
@@ -36,30 +36,28 @@ int main()
     double eucleidianDistance = 0.0;
     double manhatanDistance = 0.0;
 
-    #pragma omp parallel for reduction(+ : eucleidianDistance)
+    #pragma omp parallel for reduction(+ : eucleidianDistance) reduction(+ : manhatanDistance)
     for (int parallel_i = 0; parallel_i < N; parallel_i++)
+    {
         eucleidianDistance += (A[parallel_i] - B[parallel_i]) * (A[parallel_i] - B[parallel_i]);
-
-    #pragma omp parallel for reduction(+ : manhatanDistance)
-    for (int parallel_i = 0; parallel_i < N; parallel_i++)
         manhatanDistance += abs(A[parallel_i] - B[parallel_i]);
+    }
 
     eucleidianDistance = sqrt(eucleidianDistance);
 
     // Part 3
-    double w=0;
+    double w = 0;
     double nominatorSum = 0.0;
-    double denominatorSum = 0.0;  
+    double denominatorSum = 0.0;
 
-    #pragma omp parallel for reduction(+ : nominatorSum)
+    #pragma omp parallel for reduction(+ : nominatorSum) reduction(+ : denominatorSum)
     for (int parallel_i = 0; parallel_i < N; parallel_i++)
+    {
         nominatorSum += (A[parallel_i] * B[parallel_i]);
-
-    #pragma omp parallel for reduction(+ : denominatorSum)
-    for (int parallel_i = 0; parallel_i < N; parallel_i++)
         denominatorSum += B[parallel_i];
+    }
 
-    w = nominatorSum/ denominatorSum;
+    w = nominatorSum / denominatorSum;
 
     printf("Arrays\n");
     for (int i = 0; i < N; i++)
@@ -70,11 +68,10 @@ int main()
         printf("%d  ", B[i]);
 
     printf("\n");
-    printf("\nPart 1 Result: %f",part1Result );
-    printf("\nPart 2 eucleidianDistance: %f", eucleidianDistance );
-    printf("\nPart 2 manhatanDistance : %f", manhatanDistance );
-    printf("\nPart 3 w : %f", w );
-
+    printf("\nPart 1 Result: %f", part1Result);
+    printf("\nPart 2 eucleidianDistance: %f", eucleidianDistance);
+    printf("\nPart 2 manhatanDistance : %f", manhatanDistance);
+    printf("\nPart 3 w : %f", w);
 
     return 0;
 }
